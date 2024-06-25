e2c17065a712f15eebcc855c6e43d2732490af1d
phosda-panha <panhaphosda@gmail.com>
2024-06-25 09:53:47 +0700

    move code to gitlab

---
+++ script/diarynote-page/index.js
@@ -0,0 +1,26 @@
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.slide');
+    if (index >= slides.length) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = slides.length - 1;
+    } else {
+        currentSlide = index;
+    }
+    const offset = -currentSlide * 100;
+    document.querySelector('.slides').style.transform = `translateX(${offset}%)`;
+}
+
+function changeSlide(direction) {
+    showSlide(currentSlide + direction);
+}
+
+// Auto slide functionality (optional)
+setInterval(() => {
+    changeSlide(1);
+}, 5000); // Change image every 3 seconds
+
+// Initialize the first slide
+showSlide(currentSlide);
