cf0a78605715972ab6286d28eece7a03ea0990ee
JUST SLEEP <159119700+Alis-sda@users.noreply.github.com>
2024-07-03 22:33:50 +0700

    update

--- script/components/footer.js
+++ script/components/footer.js
@@ -3,35 +3,38 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const footerHTML = ` <link rel="stylesheet" href="/style/footer/footer.css">
         <footer class="footer">
-            <div class="footer-overlay">
-                <div class="container">
-                    <div class="footer-row">
-                        <div class="footer-col">
-                            <img src="/src/image/footer/girlprofile.png" alt="Profile Image">
-                        </div>
-                        <div class="footer-col">
-                            <h4>Contact Us</h4>
-                            <p>
-                                <i class="fas fa-phone"></i> &nbsp; +855 974053800<br>
-                                <i class="fas fa-envelope"></i> &nbsp; panhaphosda@gmail.com
-                            </p>
-                        </div>
-                        <div class="footer-col">
-                            <h4>Follow Us</h4>
-                            <div class="social-links">
-                                <a href="#"><i class="fab fa-facebook-f"></i></a>
-                                <a href="#"><i class="fab fa-telegram"></i></a>
-                                <a href="#"><i class="fab fa-instagram"></i></a>
-                                <a href="#"><i class="fab fa-gitlab"></i></a>
-                            </div>
+        <div class="footer-overlay">
+            <div class="container">
+                <div class="footer-row">
+                    <div class="footer-col ">
+                        <img src="/src/image/footer/girlprofile.png" alt="">
+                    </div>
+                    <div class="footer-col">
+                        <h4>Contact Us</h4>
+                        <p>
+                            <i class="fas fa-phone"></i> &nbsp; +855 974053800<br>
+                            <i class="fas fa-envelope"></i> &nbsp; panhaphosda@gmail.com
+                            <h1></h1>
+                        </p>
+                    </div>
+                   
+                    <div class="footer-col">
+                        <h4>Follow Us</h4>
+                        <div class="social-links">
+                            <a href="#"><i class="fab fa-facebook-f"></i></a>
+                            <a href="#"><i class="fab fa-telegram"></i></a>
+                            <a href="#"><i class="fab fa-instagram"></i></a>
+                            <a href="#"><i class="fab fa-gitlab"></i></a>
                         </div>
                     </div>
-                    <div class="footer-bottom">
-                        <p>&copy; Phosda Diary Note  |  A&B 2024</p>
-                    </div>
+                    
+                </div>
+                <div class="footer-bottom">
+                    <p>&copy; Phosda Diary Note  |  A&B 2024</p>
                 </div>
             </div>
-        </footer>
+        </div>
+    </footer>
     `;
 
     footerContainer.innerHTML = footerHTML;
--- script/components/header.js
+++ script/components/header.js
@@ -1,20 +1,23 @@
+// header.js
 document.addEventListener("DOMContentLoaded", function() {
     const headerContainer = document.getElementById("header-container");
-
-    const headerHTML = `
-    <div class="header"> 
-        <a href="">
-            <img src="/src/icons/arrowsleft.png" alt="" class="arrow-left">
-        </a>
-        <h1>Diary Note 
-            <a href="/index.html">
-                <img src="/src/image/Diarynote-page/homeicon.png" alt="home" class="home-icon">
+    const headerHTML = `<link rel="stylesheet" href="/style/header/header.css">
+        <div class="header"> 
+            <a href="">
+                <img src="/src/icons/arrowsleft.png" alt="" class="arrow-left">
             </a>
-        </h1>
-        <a href="#">
-            <img src="/src/icons/arrowsright.png" alt="" class="arrow-right">
-        </a>
-    </div>
+            <h1>Diary Note 
+                <a href="/index.html">
+                    <img src="/src/image/Diarynote-page/homeicon.png" alt="home" class="home-icon">
+                </a>
+            </h1>
+            <a href="#">
+                <img src="/src/icons/arrowsright.png" alt="" class="arrow-right">
+            </a>
+        </div>
     `;
     headerContainer.innerHTML = headerHTML;
+    document.head.appendChild(style);
+
 });
+
